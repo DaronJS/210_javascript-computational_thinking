6a95b1e047cd95fb1c5cfb4308ba32c4c20616c7
Daron Spaulding <dspaulding25@gmail.com>
2017-03-29 11:50:31 -0600

    removed function call

--- Logic_Flow_control/rot13_cipher.js
+++ Logic_Flow_control/rot13_cipher.js
@@ -15,9 +15,3 @@ function rot13(string) {
 
   return outputStr;
 }
-
-
-console.log(rot13('Teachers open the door, but you must enter by yourself.'));
-
-// logs:
-// Grnpuref bcra gur qbbe, ohg lbh zhfg ragre ol lbhefrys.
