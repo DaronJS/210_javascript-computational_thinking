728a146752a11fd51e639f0c5f24630a820b9d96
Daron Spaulding <dspaulding25@gmail.com>
2017-03-29 11:57:12 -0600

    added a constant for rotation value

--- Logic_Flow_control/rot13_cipher.js
+++ Logic_Flow_control/rot13_cipher.js
@@ -2,12 +2,13 @@
 function rot13(string) {
   var outputStr = '';
   var asciiNum;
+  var ROTATE_VAL = 13;
   for (var i = 0; i < string.length; i++) {
     asciiNum = string.charCodeAt(i);
     if ((asciiNum >= 65 && asciiNum <= 77) || (asciiNum >= 97 && asciiNum <= 109)) {
-      outputStr += String.fromCharCode(asciiNum + 13);
+      outputStr += String.fromCharCode(asciiNum + ROTATE_VAL);
     } else if ((asciiNum >= 78 && asciiNum <= 90) || (asciiNum >= 110 && asciiNum <= 122)){
-        outputStr += String.fromCharCode(asciiNum - 13);
+        outputStr += String.fromCharCode(asciiNum - ROTATE_VAL);
     } else {
         outputStr += string[i];
     }
@@ -15,3 +16,8 @@ function rot13(string) {
 
   return outputStr;
 }
+
+console.log(rot13('Teachers open the door, but you must enter by yourself.'));
+
+// logs:
+// Grnpuref bcra gur qbbe, ohg lbh zhfg ragre ol lbhefrys.
