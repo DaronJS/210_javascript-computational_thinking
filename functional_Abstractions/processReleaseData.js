91f7172a31a09b5f7f2a8254d6bca28f6ae1fc13
Daron Spaulding <dspaulding25@gmail.com>
2017-04-26 12:24:37 -0600

    adding another exercise in section

---
+++ functional_Abstractions/processReleaseData.js
@@ -0,0 +1,46 @@
+// logic
+// filter the objects for objects that contain id and title properties
+// return an array of new objects that contain only the id and title properties from the original objects
+function processReleaseData(data) {
+  return data.filter(function(obj) {
+    var properties = Object.keys(obj);
+    return properties.includes('id') && properties.includes('title');
+  }).map(function (obj) {
+    return {id: obj['id'], title: obj['title']};
+  });
+}
+
+var newReleases = [
+  {
+    'id': 70111470,
+    'title': 'Die Hard',
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/DieHard.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [4.0],
+    'bookmark': [],
+  },
+  {
+    'id': 654356453,
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/BadBoys.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [5.0],
+    'bookmark': [{ id:432534, time:65876586 }],
+  },
+  {
+    'title': 'The Chamber',
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/TheChamber.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [4.0],
+    'bookmark': [],
+  },
+  {
+    'id': 675465,
+    'title': 'Fracture',
+    'boxart': 'http://cdn-0.nflximg.com/images/2891/Fracture.jpg',
+    'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+    'rating': [5.0],
+    'bookmark': [{ id:432534, time:65876586 }],
+  },
+];
+
+console.log(processReleaseData(newReleases));
